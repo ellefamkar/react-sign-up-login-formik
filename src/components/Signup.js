75d9836cd378cd00fbdc502b14f978883f07b82e
elnaz famkar <86571329+ellefamkar@users.noreply.github.com>
2023-01-15 20:18:41 +0330

    feat : add formik validate, touched and onSubmit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,46 +1,76 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { useFormik } from 'formik';
 
 // 1.manage state 
 // 2.handle form submission 
 // 3.validation - error message 
 // 4. formik --> use formik hook --> useFormik
 
+// 1. 
+const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+    // confirmPassword: "",
+    // select: "",
+    // isChecked: "" 
+}
+
+// 2. 
+const onSubmit = (values) =>{
+    console.log(values);
+}
+
+// 3. 
+const validate = (values) =>{
+    let errors = {};
+    
+    if(!values.name){
+        errors.name = "Name is required";
+    }
+
+    
+    if(!values.email){
+        errors.email = "Email is required";
+    }
+
+    
+    if(!values.password){
+        errors.password = "Password is required";
+    }
+
+    return errors;
+}
+
 const Signup = () => {
+
     // name, pass, email => submit => DB => redirect /panel 
-    const [data, setData] = useState({
-        name: "",
-        email:"",
-        // password: ""
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
     });
 
-    const handleChange = ({target}) =>{
-        setData({
-            ...data,
-            [target.name] : target.value
-        });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // call the server => submit data => post => user data
-        console.log(event);
-    };
+    console.log(formik.errors);
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" value={data.name} onChange={handleChange} />
+                    <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                    {formik.errors.name && formik.touched.name && <span>{formik.errors.name}</span> }
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="email" name="email" value={data.email} onChange={handleChange} />
+                    <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                    {formik.errors.email && formik.touched.email && <span>{formik.errors.email}</span> }
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" value={data.password} onChange={handleChange} />
+                    <input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                    {formik.errors.password && formik.touched.password && <span>{formik.errors.password}</span> }
                 </div>
                 <div>
                     <button type='submit'>
