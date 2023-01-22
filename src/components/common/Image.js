3df211ff4d8940b223e14fdb2c0d19d5d5df8fe8
elnaz famkar <86571329+ellefamkar@users.noreply.github.com>
2023-01-22 19:33:39 +0330

    feat: add Animated-Fluent-Emojis for background

--- src/components/common/Image.js
+++ src/components/common/Image.js
@@ -5,6 +5,7 @@ import styles from './Image.module.css';
 const Image = () => {
     return (
         <div className={styles.ImageContainer}>
+            <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20professions/Man%20Technologist%20Light%20Skin%20Tone.png" alt="Man Technologist Light Skin Tone" width="25" height="25" />
             {/* <img src={UserProfile} alt="UserProfile" /> */}
         </div>
     );
